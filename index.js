eb548c06b9338a6fd27bb0db6ed02907d88efe9e
harshithareddy123456 <vsharshi233@gmail.com>
2024-04-01 16:13:41 +0530

    gofood-backend

--- index.js
+++ index.js
@@ -8,24 +8,14 @@ mongoDB();
 // CORS middleware
 app.use(
   cors({
-    origin: "https://merngofood-frontend-1.onrender.com/",
+    origin: [
+      "http://localhost:3000",
+      "https://merngofood-frontend-1.onrender.com",
+    ],
     methods: ["POST", "GET"], // Note: Use 'methods' instead of 'method'
     credentials: false,
   })
 );
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://merngofood-backend-1.onrender.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(express.json());
 
