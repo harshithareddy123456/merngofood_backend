5c1a58589bfdad0a83140d7f131723522e21948b
harshithareddy123456 <vsharshi233@gmail.com>
2024-04-09 18:39:44 +0530

    vercel

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -9,7 +9,6 @@ const jwtSecret = "qwertyuioo";
 
 router.post(
   "/createuser",
-  cors({ origin: "https://merngofood-frontend.vercel.app/" }),
   [
     body("email").isEmail(),
     body("name").isLength({ min: 5 }),
@@ -41,43 +40,38 @@ router.post(
   }
 );
 
-router.post(
-  "/loginuser",
-  cors({ origin: "https://merngofood-frontend.vercel.app/" }),
-  [body("email").isEmail()],
-  async (req, res) => {
-    try {
-      const result = validationResult(req);
-      if (!result.isEmpty()) {
-        return res.status(400).json({ errors: result.array() });
-      }
-      const { email, password } = req.body;
-      let userData = await User.findOne({ email });
-      if (!userData) {
-        return res
-          .status(400)
-          .json({ errors: "Try logging in with correct credentials" });
-      }
-      const pwdCompare = await bcrypt.compare(
-        req.body.password,
-        userData.password
-      );
-      if (!pwdCompare) {
-        return res
-          .status(400)
-          .json({ errors: "Try logging in with correct credentials" });
-      }
-      const data = {
-        user: {
-          id: userData.id,
-        },
-      };
-      const authToken = jwt.sign(data, jwtSecret);
-      return res.json({ success: true, authToken: authToken });
-    } catch (error) {
-      console.log("error");
-      res.json({ success: false });
+router.post("/loginuser", [body("email").isEmail()], async (req, res) => {
+  try {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
     }
+    const { email, password } = req.body;
+    let userData = await User.findOne({ email });
+    if (!userData) {
+      return res
+        .status(400)
+        .json({ errors: "Try logging in with correct credentials" });
+    }
+    const pwdCompare = await bcrypt.compare(
+      req.body.password,
+      userData.password
+    );
+    if (!pwdCompare) {
+      return res
+        .status(400)
+        .json({ errors: "Try logging in with correct credentials" });
+    }
+    const data = {
+      user: {
+        id: userData.id,
+      },
+    };
+    const authToken = jwt.sign(data, jwtSecret);
+    return res.json({ success: true, authToken: authToken });
+  } catch (error) {
+    console.log("error");
+    res.json({ success: false });
   }
-);
+});
 module.exports = router;
--- Routes/DisplayData.js
+++ Routes/DisplayData.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.post(
-  "/foodData",
-  cors({ origin: "https://merngofood-frontend.vercel.app/" }),
-  (req, res) => {
-    try {
-      res.send([global.foodItems, global.foodCategories]);
-    } catch (error) {
-      console.error(error.messege);
-      res.send("Server error");
-    }
+router.post("/foodData", (req, res) => {
+  try {
+    res.send([global.foodItems, global.foodCategories]);
+  } catch (error) {
+    console.error(error.messege);
+    res.send("Server error");
   }
-);
+});
 
 module.exports = router;
